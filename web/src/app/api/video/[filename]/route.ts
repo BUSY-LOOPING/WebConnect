252e4578c65b01bc4425285b690c58f49849cc69
Dhruv <dhruvyadav2905@gmail.com>
2026-03-08 04:29:25 -0400

    fixed video src proxy

---
+++ web/src/app/api/video/[filename]/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from 'next/server'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+
+const s3 = new S3Client({
+    endpoint: process.env.MINIO_ENDPOINT, 
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY!,
+        secretAccessKey: process.env.SECRET_KEY!,
+    },
+    region: process.env.BUCKET_REGION,
+    forcePathStyle: true
+})
+
+export async function GET(req: NextRequest, { params }: { params: { filename: string } }) {
+    const command = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME!,
+        Key: params.filename,
+    })
+
+    const s3Response = await s3.send(command)
+    const stream = s3Response.Body as ReadableStream
+
+    return new Response(stream, {
+        headers: {
+            'Content-Type': 'video/webm',
+            'Cache-Control': 'public, max-age=86400',
+        }
+    })
+}
